e58f0c69f8e048c349ae82df472b60fba2740fc1
SergioSalinas <salinassierrasergio05@email.com>
2025-09-25 14:07:39 +0200

    si

--- ejercicioPrueba/src/ejercicioPrueba/controlador/vistaControlador/TipoController.java
+++ ejercicioPrueba/src/ejercicioPrueba/controlador/vistaControlador/TipoController.java
@@ -1,10 +1,38 @@
 package ejercicioPrueba.controlador.vistaControlador;
 
-public class TipoController implements ITipoController{
+import ejercicioPrueba.modelo.tipo.TipoModel;
 
+public class TipoController implements ITipoController{
+	 private TipoModel tipoModel;
+	 public TipoController() {
+		 this.tipoModel = new TipoModel();
+	 }
 	@Override
 	public void controlMenu(int choose) {
-		// TODO Auto-generated method stub
+		switch (choose) {
+		case 1:
+				//insertar tipos
+				
+			break;
+		case 2:
+				//listar tipos
+			
+			break;
+		case 3:
+				//update tipos
+			break;
+		case 4:	
+				// ir a empleado
+			break;
+		case 5:	
+			// ir a usuario
+		break;
+		case 6:	
+			// ir a promoción
+		break;
+		default:
+			break;
+		}
 		
 	}
 
---
+++ ejercicioPrueba/src/ejercicioPrueba/modelo/tipo/ITipoModel.java
@@ -0,0 +1,5 @@
+package ejercicioPrueba.modelo.tipo;
+
+public interface ITipoModel {
+
+}
