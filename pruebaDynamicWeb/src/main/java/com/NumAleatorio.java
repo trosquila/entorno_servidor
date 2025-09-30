c30574a93a74aaa4012011ebb5e394cf7f5efee4
SergioSalinas <salinassierrasergio05@email.com>
2025-09-30 12:03:25 +0200

    [add] se añadio ejercicio de ejemplo y ejercicio para entrega

---
+++ pruebaDynamicWeb/src/main/java/com/NumAleatorio.java
@@ -0,0 +1,47 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class NumAleatorio
+ */
+@WebServlet("/NumAleatorio")
+public class NumAleatorio extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NumAleatorio() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		Double numAleatorio = Math.random();
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>");
+		out.print("<h3>Aqui tienes tu número: "+numAleatorio+"</h3>");
+		out.print("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
