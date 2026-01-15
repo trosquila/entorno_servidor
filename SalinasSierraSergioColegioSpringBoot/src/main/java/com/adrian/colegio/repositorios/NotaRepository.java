449ac2cb3027e20bfabcd233ccbe5739eae8946d
Sergio Salinas <correoSergio@falso.com>
2026-01-15 23:39:34 +0100

    lista

--- SalinasSierraSergioColegioSpringBoot/src/main/java/com/adrian/colegio/repositorios/NotaRepository.java
+++ SalinasSierraSergioColegioSpringBoot/src/main/java/com/adrian/colegio/repositorios/NotaRepository.java
@@ -11,24 +11,23 @@ import com.adrian.colegio.entities.NotaEntity;
 
 public interface NotaRepository extends CrudRepository<NotaEntity, Integer> {
 
-    @Query("""
-        SELECT new com.adrian.colegio.dtos.NotaDTO(
-            n.id,
-            n.alumno.id,
-            n.asignatura.id,
-            n.alumno.nombre,
-            n.asignatura.nombre,
-            n.nota,
-            n.fecha
-        )
-        FROM NotaEntity n
-        WHERE (:idNota IS NULL OR n.id = :idNota)
-          AND (:idAlumno IS NULL OR n.alumno.id = :idAlumno)
-          AND (:idAsignatura IS NULL OR n.asignatura.id = :idAsignatura)
-    """)
-    ArrayList<NotaDTO> buscarNotas(
-        @Param("idNota") Integer idNota,
-        @Param("idAlumno") Integer idAlumno,
-        @Param("idAsignatura") Integer idAsignatura
-    );
+	@Query("""
+		    SELECT new com.adrian.colegio.dtos.NotaDTO(
+		        n.alumno.id,
+		        n.alumno.nombre,
+		        n.asignatura.nombre,
+		        n.nota,
+		        n.fecha
+		    )
+		    FROM NotaEntity n
+		    WHERE (:idNota IS NULL OR n.id = :idNota)
+		      AND (:idAlumno IS NULL OR n.alumno.id = :idAlumno)
+		      AND (:idAsignatura IS NULL OR n.asignatura.id = :idAsignatura)
+		""")
+		ArrayList<NotaDTO> buscarNotas(
+		    @Param("idNota") Integer idNota,
+		    @Param("idAlumno") Integer idAlumno,
+		    @Param("idAsignatura") Integer idAsignatura
+		);
+
 }
